1824425bc8d685519bbfdaccd823cc11f3820939
ria-galang <ria.galang21@gmail.com>
2023-03-04 12:30:08 -0500

    added AddingNumbers class

---
+++ LearnJava/src/actionItems/AddingNumbers.java
@@ -0,0 +1,31 @@
+package actionItems;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+		
+		//for loop
+		int sum = 0;
+		for(int i = 0; i<=15; i++)
+		{
+		sum = sum + i;
+		}
+		
+		System.out.println("The result is : " + sum);
+		
+		
+		//while loop
+		int sum1 = 0;
+		int i = 0;
+		while (i<=15)
+		{
+			sum1 = sum1 + i;
+			i++;
+		}
+		System.out.println("The result is : " + sum1);
+		
+	
+	
+	}
+
+}
